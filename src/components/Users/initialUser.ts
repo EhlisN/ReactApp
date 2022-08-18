60c06c22e880db9c8a5c25ad5421513ad7f6a94e
NatalliaE <natasha.ehlis@gmail.com>
2022-08-18 18:39:53 +0300

    Add new project

---
+++ src/components/Users/initialUser.ts
@@ -0,0 +1,7 @@
+export const initialUser = {
+    "id": 13,
+    "email": '',
+    "first_name": '',
+    "last_name": '',
+    "avatar": 'https://klike.net/uploads/posts/2019-07/1564314090_3.jpg'
+}
